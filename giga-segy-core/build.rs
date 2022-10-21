d802bea6a147da715d3cf18d0f6e891a37e1a98b
Björn Wieczoreck <bjoern.wieczoreck@giga-infosystems.com>
2022-10-21 12:20:41 +0200

    using a better name for the header file

--- giga-segy-core/build.rs
+++ giga-segy-core/build.rs
@@ -5,7 +5,7 @@ fn main() {
         use std::env;
         let root = env::var("CARGO_MANIFEST_DIR").unwrap();
         let _ = cbindgen::generate(&root)
-            .map(|h| h.write_to_file("include/rsg_core.h"))
+            .map(|h| h.write_to_file("include/giga-segy.h"))
             .map_err(|error_message| println!("cargo:warning={}", error_message));
 
         // the following generates an environment variable `DEP__RUST_SEGY_GIGA_INCLUDE`

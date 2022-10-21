b261d921cced1f857b2985a8ded083caf16c2a2f
Aleksey Zholobenko <aleksey.zholobenko@giga-infosystems.com>
2022-10-21 11:22:08 +0200

    Revert "Modify `build.rs`:"
    This reverts commit ab6ddd82b99a49eef9cbad1fd9955a0e2619d5d9.

--- giga-segy-core/build.rs
+++ giga-segy-core/build.rs
@@ -1,28 +1,15 @@
 extern crate cbindgen;
-use std::env;
-use std::path::PathBuf;
 
 fn main() {
-    let root = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let _ = cbindgen::generate(&root)
-        .map(|h| h.write_to_file("include/giga_segy.h"))
-        .map_err(|error_message| println!("cargo:warning={}", error_message));
+    {
+        use std::env;
+        let root = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let _ = cbindgen::generate(&root)
+            .map(|h| h.write_to_file("include/rsg_core.h"))
+            .map_err(|error_message| println!("cargo:warning={}", error_message));
 
-    // Recently generation of the variabe `DEP__RUST_SEGY_GIGA_INCLUDE` has not been reliable,
-    // therefore we copy the header directly to the build path (`target/debug` or `target/release`).
-    let mut target = match env::var("OUT_DIR") {
-        Ok(o) => PathBuf::from(o),
-        Err(e) => panic!("cargo:warning=Error when copying \"giga_segy.h\": {}", e),
-    };
-    target.pop();
-    target.pop();
-    target.pop();
-
-    let src = format!("{:?}/include/giga_segy.h", root.as_str());
-    let src = src.replace('\"', "");
-    let dest = format!("{:?}/giga_segy.h", target);
-    let dest = dest.replace('\"', "");
-    if let Err(e) = std::fs::copy(src, dest) {
-        panic!("cargo:warning=Error when copying \"giga_segy.h\": {}", e);
+        // the following generates an environment variable `DEP__RUST_SEGY_GIGA_INCLUDE`
+        // the double `_` is due to having `links = ""` set empty in `cargo.toml`
+        println!("cargo:RUST_SEGY_GIGA_INCLUDE={}/include", root.as_str());
     }
 }
